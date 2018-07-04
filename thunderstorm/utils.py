03c03825a484ca95a30c2c86e1e49cdca6bd951b
shipperizer <alexcabb@gmail.com>
2018-07-04 14:38:19 +0100

    [release] paginate in v0.1.8

--- thunderstorm/utils.py
+++ thunderstorm/utils.py
@@ -31,3 +31,26 @@ def get_pagination_info(page, page_size, num_records, url_path):
 
     pagination_info['total_records'] = num_records
     return pagination_info
+
+
+def paginate(query, page, page_size, url_path):
+    """
+    Take a sqlalchemy query and paginate it based on the args provided.
+
+    Args:
+        query (sqlalchemy Query object): Query you wish to paginate
+        page (int): Page number of the results page to return
+        page_size (int): Number of results to return per page
+        url_path (str): url_path that the request was sent to e.g. for the url
+            'http://localhost/foo/bar/baz/' it would be '/foo/bar/baz'.
+            Query parameters are stripped out by the get_pagination_info func.
+
+    Returns:
+        tuple: (Paginated Query object applied to it, dict of pagination info)
+    """
+    start = (page - 1) * page_size
+    num_records = query.count()
+
+    pagination_info = get_pagination_info(page, page_size, num_records, url_path)
+
+    return query.offset(start).limit(page_size), pagination_info
