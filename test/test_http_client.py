9483fac1daff49df3e6cc38a238207ec9c1b8dd1
lycheng <lycheng997@gmail.com>
2019-10-15 19:36:50 +0800

    test: init benchmark in http_client

--- test/test_http_client.py
+++ test/test_http_client.py
@@ -5,6 +5,17 @@ import pytest
 
 from thunderstorm.http_client import (SessionPool, HttpClient)
 
+urls = [
+    "https://www.baidu.com",
+    "https://www.qq.com",
+    "https://www.taobao.com",
+]
+
+
+@pytest.fixture(scope="session")
+def http_client():
+    return HttpClient()
+
 
 def test_session_pool():
     pool = SessionPool(2)
@@ -35,10 +46,20 @@ def test_http_client():
 
 def test_http_client_with_multi_urls():
     http_client = HttpClient(1)
-    urls = [
-        "https://www.baidu.com",
-        "https://www.qq.com",
-        "https://www.taobao.com",
-    ]
     for url in urls:
         assert http_client.get(url).status_code == 200
+
+
+def test_requests_get(benchmark, http_client):
+
+    @benchmark
+    def request_mulit_url():
+        for url in urls:
+            http_client.get(url)
+
+
+def test_http_client_get(benchmark):
+    @benchmark
+    def request_mulit_url():
+        for url in urls:
+            requests.get(url)
