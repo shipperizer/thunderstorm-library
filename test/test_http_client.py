75829db073ae27bcbc1beb0f7dfa5ad20bdb8ecd
lycheng <lycheng997@gmail.com>
2019-10-16 19:14:36 +0800

    test: update http client unit tests

--- test/test_http_client.py
+++ test/test_http_client.py
@@ -9,18 +9,9 @@ from thunderstorm.http_client import (SessionPool, HttpClient)
 
 @pytest.fixture(scope="session")
 def urls():
-    base_urls = [
-        'https://www.baidu.com',
-        'https://www.qq.com',
-        'https://www.taobao.com',
-        'https://www.alibaba.com'
+    rv = [
+        'https://www.alibaba.com' for _ in range(20)
     ]
-
-    rv = []
-    for url in base_urls:
-        rv.extend([url] * random.randint(5, 10))
-
-    random.shuffle(rv)
     return rv
 
 
@@ -35,10 +26,9 @@ def test_session_pool():
     session = pool.get()
     assert isinstance(session, requests.Session)
 
-    _ = pool.get()
-
+    pool.get()
     with pytest.raises(queue.Empty):
-        _ = pool.get(False)
+        pool.get(False)
 
 
 def test_http_client():
@@ -49,11 +39,35 @@ def test_http_client():
     r = http_client.get(url)
     assert r.status_code == 200
 
+    url = "http://httpbin.org/status/404"
+    r = http_client.get(url)
+    assert r.status_code == 404
+
     url = "http://httpbin.org/delay/2"
     with pytest.raises(requests.exceptions.Timeout):
         http_client.get(url, timeout=1)
 
     assert http_client.sessions.size() == 1
+
+
+def test_http_client_reuqest_multi_method():
+    http_client = HttpClient(1)
+    assert http_client.sessions.size() == 1
+
+    url = "http://httpbin.org/status/200"
+    methods = [
+        "get",
+        "options",
+        "head",
+        "post",
+        "put",
+        "patch",
+        "delete"
+    ]
+
+    for m in methods:
+        f = getattr(http_client, m)
+        assert f(url).status_code == 200
 
 
 def test_http_client_with_multi_urls(urls):
