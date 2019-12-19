7020a2f7998c3160590312215d7b48a6b2f06e76
JayZhou@vm_ubuntu <jayzml@gmail.com>
2019-12-19 18:06:53 +0800

    [release] add metronome

---
+++ thunderstorm/utils/metronome.py
@@ -0,0 +1,23 @@
+import time
+import binascii
+
+
+class Metronome(object):
+    """
+    节拍器
+    """
+
+    def __init__(self, tick_time, beat_times_every_day):
+        """
+        tick_time: int (minutes) trigger every n minutes
+        beat_times_every_day: int  the func should run n times every day
+        """
+        self.fetch_times_factor = int(24 * 60 * 60 / beat_times_every_day / (tick_time * 60))
+        self.now_remainder = int(time.time() / (tick_time * 60)) % self.fetch_times_factor
+
+    def beat_it(self, uuid):
+        crc = binascii.crc32(uuid.bytes)
+        obj_remainder = crc % self.fetch_times_factor
+        if self.now_remainder == obj_remainder:
+            return True
+        return False
